f0b4b25ee925fa6396b5d52777cd2ab53acfbac5
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2022-12-17 10:24:47 +0700

    Part 1 done, part 2 memoization mem error

--- 2022/Day-16/main.py
+++ 2022/Day-16/main.py
@@ -2,35 +2,36 @@
 
 from fnmatch import fnmatch
 
-INPUT = "input.txt"
+INPUT = "test.txt"
 
 
-def part_1():
-    def floyd_warshall(valve_map: dict[str, list[str]]) -> dict[str, dict[str, int]]:
-        min_dist = {valve: {} for valve in valve_map}
+def floyd_warshall(valve_map: dict[str, list[str]]) -> dict[str, dict[str, int]]:
+    min_dist = {valve: {} for valve in valve_map}
 
-        for valve in valve_map:
-            for other_valve in valve_map:
-                min_dist[valve][other_valve] = (
-                    0
-                    if valve == other_valve
-                    else 1
-                    if other_valve in valve_map[valve][1]
-                    else float("inf")
+    for valve in valve_map:
+        for other_valve in valve_map:
+            min_dist[valve][other_valve] = (
+                0
+                if valve == other_valve
+                else 1
+                if other_valve in valve_map[valve][1]
+                else float("inf")
+            )
+
+    for mid in valve_map:
+        for origin in valve_map:
+            for dest in valve_map:
+                min_dist[origin][dest] = min(
+                    min_dist[origin][dest],
+                    min_dist[origin][mid] + min_dist[mid][dest],
                 )
 
-        for mid in valve_map:
-            for origin in valve_map:
-                for dest in valve_map:
-                    min_dist[origin][dest] = min(
-                        min_dist[origin][dest],
-                        min_dist[origin][mid] + min_dist[mid][dest],
-                    )
+    return min_dist
 
-        return min_dist
 
-    with open(INPUT) as file:
-        valve_map: dict[str, list[str]] = {}
+def parse_input(input_file) -> tuple[dict[str, dict[str, int]], dict[str, int]]:
+    with open(input_file) as file:
+        valve_map: dict[str, list[int, str]] = {}
         for line in file:
             if fnmatch(line, "Valve ?? has flow rate=*; tunnels lead to valves *"):
                 valve, dest = line[:-1].split("; ")
@@ -54,35 +55,114 @@ def part_1():
 
             valve_map[valve] = [rate, dest]
 
-    min_dist = floyd_warshall(valve_map)
-    activated = set()
-    t_minus = 30
-    current_valve = "AA"
-    pressure = 0
-    while t_minus > 0 and len(activated) < len(valve_map):
-        while (
-            nxt_best_valve := max(
-                min_dist[current_valve],
-                key=lambda valve: (t_minus - min_dist[current_valve][valve] - 1)
-                * valve_map[valve][0],
-            )
-        ) in activated:
-            min_dist[current_valve].pop(nxt_best_valve)
+    return (
+        floyd_warshall(valve_map),
+        {valve: valve_map[valve][0] for valve in valve_map},
+    )
 
-        if valve_map[nxt_best_valve][0] == 0:
-            break
 
-        t_minus -= min_dist[current_valve][nxt_best_valve] + 1
-        if t_minus > 0:
-            activated.add(nxt_best_valve)
-            pressure += t_minus * valve_map[nxt_best_valve][0]
-            current_valve = nxt_best_valve
+def part_1():
+    T_MINUS = 30
 
-    return pressure, activated
+    def dfs_valves(
+        valves_explored: list[str],
+        valves_remaining: set[str],
+        time_remaining: int,
+        explored_pressure: int,
+    ):
+        if tuple(valves_explored) not in pressure:
+            pressure[tuple(valves_explored)] = explored_pressure
+
+        curr_valve = valves_explored[-1]
+        for valve in sorted(
+            valves_remaining,
+            key=min_dist[curr_valve].get,
+        ):
+            if (new_time := time_remaining - min_dist[curr_valve][valve] - 1) > 0:
+                new_explored = valves_explored + [valve]
+                if tuple(new_explored) not in pressure:
+                    dfs_valves(
+                        new_explored,
+                        valves_remaining.difference(new_explored),
+                        new_time,
+                        explored_pressure + valve_map[valve] * new_time,
+                    )
+                pressure[tuple(valves_explored)] = max(
+                    pressure[tuple(new_explored)],
+                    pressure[tuple(valves_explored)],
+                )
+            else:
+                break
+
+    min_dist, valve_map = parse_input(INPUT)
+    pressure = {}
+    dfs_valves(
+        ["AA"], set(valve for valve in valve_map if valve_map[valve] > 0), T_MINUS, 0
+    )
+
+    return pressure[("AA",)]
 
 
 def part_2():
-    pass
+    T_MINUS = 26
+
+    def dfs_valves(
+        valves_explored: list[str] = ["AA"],
+        old_explored: list[str] | None = None,
+        valves_remaining: set[str] = set(),
+        time_remaining: int = T_MINUS,
+        explored_pressure: int = 0,
+    ):
+        path = (
+            tuple(valves_explored)
+            if not old_explored
+            else (tuple(old_explored), tuple(valves_explored))
+        )
+        if path not in pressure:
+            pressure[path] = explored_pressure
+
+        curr_valve = valves_explored[-1]
+        for valve in sorted(
+            valves_remaining,
+            key=min_dist[curr_valve].get,
+        ):
+            if (new_time := time_remaining - min_dist[curr_valve][valve] - 1) > 0:
+                new_explored = valves_explored + [valve]
+                child_path = (
+                    tuple(new_explored)
+                    if not old_explored
+                    else (tuple(old_explored), tuple(new_explored))
+                )
+                if child_path not in pressure:
+                    dfs_valves(
+                        new_explored,
+                        old_explored,
+                        valves_remaining.difference(new_explored),
+                        new_time,
+                        explored_pressure + valve_map[valve] * new_time,
+                    )
+
+                pressure[path] = max(pressure[child_path], pressure[path])
+            else:
+                break
+
+        if not old_explored:
+            child_path = (path, ("AA",))
+            if child_path not in pressure:
+                dfs_valves(
+                    old_explored=valves_explored,
+                    valves_remaining=valves_remaining,
+                    explored_pressure=explored_pressure,
+                )
+            pressure[path] = max(pressure[path], pressure[child_path])
+
+    min_dist, valve_map = parse_input(INPUT)
+    pressure = {}
+    dfs_valves(
+        valves_remaining=set(valve for valve in valve_map if valve_map[valve] > 0)
+    )
+
+    return pressure[("AA",)]
 
 
 print(f"{part_1() = }")
