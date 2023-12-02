9c6116464e6e1a2f6ec32f883e458725a9b46447
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2023-12-02 12:41:42 +0200

    2023 Day 2

---
+++ 2023/Day-2/main.py
@@ -0,0 +1,43 @@
+def parse_data(raw: str):
+    INDEX = {"red": 0, "green": 1, "blue": 2}
+
+    def parse_line(line: str):
+        game, turns = map(lambda part: part.strip(), line.split(":"))
+
+        parsed_line: list[int | list[int]] = [int(game.split(" ")[1])]
+
+        for stage in map(lambda part: part.strip(), turns.split(";")):
+            pull = [0, 0, 0]
+            for ball in map(lambda part: part.strip(), stage.split(",")):
+                quant, color = ball.split(" ")
+                pull[INDEX[color]] = int(quant)
+            parsed_line.append(pull)
+        return parsed_line
+
+    return list(map(parse_line, raw.splitlines()))
+
+def part_1(games: list[list[int | list[int]]]):
+    MAX_RGB = (12, 13, 14)
+    def is_possible(game: list[int | list[int]]):
+        return all(map(lambda rgb: all(color <= max_color for color, max_color in zip(rgb, MAX_RGB)), game[1:]))
+
+    return sum(map(lambda game: game[0], filter(is_possible, games)))
+
+def part_2(games: list[list[int | list[int]]]):
+    def get_power(stages: list[list[int]]) -> int:
+        from math import prod
+        all_rgb = list(zip(*stages))
+        return prod((max(all_rgb[0]), max(all_rgb[1]), max(all_rgb[2])))
+
+    return sum(map(get_power, map(lambda game: game[1:], games)))
+
+def main():
+    with open("input.txt") as file:
+        games = parse_data(file.read())
+    print(part_1(games))
+    print(part_2(games))
+
+
+
+if __name__ == "__main__":
+    main()
