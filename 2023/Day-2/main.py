5835b79757e5d2fab879191689fb5b36c3373548
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2023-12-04 22:15:57 +0200

    Simplify parse_data return format

--- 2023/Day-2/main.py
+++ 2023/Day-2/main.py
@@ -1,15 +1,15 @@
-def parse_data(raw: str):
+def parse_data(raw: str) -> list[list[list[int]]]:
     INDEX = {"red": 0, "green": 1, "blue": 2}
 
     def parse_line(line: str):
-        game, turns = map(lambda part: part.strip(), line.split(":"))
+        _, turns = map(lambda part: part.strip(), line.split(":"))
 
-        parsed_line: list[int | list[int]] = [int(game.split(" ")[1])]
+        parsed_line: list[list[int]] = []
 
         for stage in map(lambda part: part.strip(), turns.split(";")):
             pull = [0, 0, 0]
             for ball in map(lambda part: part.strip(), stage.split(",")):
-                quant, color = ball.split(" ")
+                quant, color = ball.split()
                 pull[INDEX[color]] = int(quant)
             parsed_line.append(pull)
         return parsed_line
@@ -17,19 +17,18 @@ def parse_data(raw: str):
     return list(map(parse_line, raw.splitlines()))
 
 
-def part_1(games: list[list[int | list[int]]]):
+def part_1(games: list[list[list[int]]]) -> int:
     MAX_RGB = (12, 13, 14)
 
-    def is_possible(game: list[int | list[int]]):
-        return all(map(lambda rgb: all(color <= max_color for color, max_color in zip(rgb, MAX_RGB)), game[1:]))
+    def is_possible(game: tuple[int, list[list[int]]]) -> bool:
+        return all(map(lambda rgb: all(color <= max_color for color, max_color in zip(rgb, MAX_RGB)), game[1]))
 
-    return sum(map(lambda game: game[0], filter(is_possible, games)))
+    return sum(indx for indx, _ in filter(is_possible, enumerate(games, start=1)))
 
 
-def part_2(games: list[list[int | list[int]]]):
+def part_2(games: list[list[list[int]]]) -> int:
     from math import prod
-    return sum(map(lambda stages: prod(map(max, zip(*stages))),
-                   map(lambda game: game[1:], games)))
+    return sum(map(lambda stages: prod(map(max, zip(*stages))), games))
 
 
 def main():
