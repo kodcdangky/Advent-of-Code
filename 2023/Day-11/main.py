9a935df1b7f7bd051e6e94ca2e145277f96bd65d
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2023-12-14 08:23:01 +0700

    Optimized day 11 for time

--- 2023/Day-11/main.py
+++ 2023/Day-11/main.py
@@ -1,24 +1,27 @@
 def parse_data(raw: str):
-    return list(map(lambda line: list(line), raw.splitlines()))
+    return raw.splitlines()
 
 
-def part_1(data: list[list[str]]):
+def part_1(data: list[str]):
     return part_2(data, 2)
 
 
 # Not very space efficient but it does the job
-def part_2(data: list[list[str]], expand_factor: int):
-    def count_empty(start_indx: int, end_indx: int, row: bool) -> int:
+def part_2(data: list[str], expand_factor: int):
+    def count_empty(start: int, end: int, row: bool) -> int:
         from bisect import bisect
-        min_indx, max_indx = min(start_indx, end_indx), max(start_indx, end_indx)
+
+        min_indx, max_indx = min(start, end), max(start, end)
         dimen = (row and empty_rows) or empty_cols
         return bisect(dimen, max_indx) - bisect(dimen, min_indx)
 
-    transposed_data = list(list(row[col_indx] for row in data)
-                           for col_indx, _ in enumerate(data[0]))
+    transposed_data = list("".join(row[col_indx] for row in data)
+                           for col_indx in range(len(data[0])))
 
-    empty_rows = list(row_indx for row_indx, row in enumerate(data) if "#" not in row)
-    empty_cols = list(col_indx for col_indx, col in enumerate(transposed_data) if "#" not in col)
+    empty_rows = list(row_indx for row_indx, row in enumerate(data)
+                      if "#" not in row)
+    empty_cols = list(col_indx for col_indx, col in enumerate(transposed_data)
+                      if "#" not in col)
 
     galaxies = list((row_indx, col_indx)
                     for row_indx, row in enumerate(data)
